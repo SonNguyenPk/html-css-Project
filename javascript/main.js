66d06f19114a3016530db1fad4bfeff5a8da2cf8
Sonnguyen <sonnguyen.pk1301@gmail.com>
2020-09-02 13:47:05 +0700

    fix javascript and adjusted gap in content

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,17 @@
-function showmenu() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+// function showmenu() {
+//     var x = document.getElementById("menu");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
+
+
+const nav = document.querySelector('#menu');
+const button = document.querySelector('#showmenu');
+if (button) {
+    button.addEventListener('click', (e) => {
+        nav.classList.toggle('show-hide');
+    });
 }
