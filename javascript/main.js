d16d515978d5f0ead91d5aa0bd08a36ffc564365
Sonnguyen <sonnguyen.pk1301@gmail.com>
2021-05-09 09:12:16 +0700

    fix name of file in js folder

--- javascript/main.js
+++ javascript/main.js
@@ -1,30 +1,33 @@
-const nav = document.querySelector('#menu');
-const button = document.querySelector('#showmenu');
+const nav = document.querySelector("#menu");
+const button = document.querySelector("#showmenu");
 if (button) {
-    button.addEventListener('click', (e) => {
-        nav.classList.toggle('show-hide');
-        button.classList.toggle('button--active');
-    });
+	button.addEventListener("click", (e) => {
+		nav.classList.toggle("show-hide");
+		button.classList.toggle("button--active");
+	});
 }
 
 window.onscroll = function () {
-    scrollFunction()
+	scrollFunction();
 };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        document.getElementById("scroll-top").style.display = "block";
-    } else {
-        document.getElementById("scroll-top").style.display = "none";
-    }
+	if (
+		document.body.scrollTop > 100 ||
+		document.documentElement.scrollTop > 100
+	) {
+		document.getElementById("scroll-top").style.display = "block";
+	} else {
+		document.getElementById("scroll-top").style.display = "none";
+	}
 }
 
-const scrolltotop = document.querySelector('#scroll-top');
-if (scrolltotop) {
-    scrolltotop.addEventListener('click', (e) => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
+const scrollToTop = document.querySelector("#scroll-top");
+if (scrollToTop) {
+	scrollToTop.addEventListener("click", (e) => {
+		window.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+	});
 }
